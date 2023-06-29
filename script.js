30a5da8b5a862837f6951fbbea1b0035d0ac4e16
WyattBensman <wyattbensman5@gmail.com>
2023-06-29 17:31:49 -0400

    Confirmed the prompts & added them to an array to be selected from

--- script.js
+++ script.js
@@ -6,22 +6,49 @@ const upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const specialChars = [" ", "!", ".", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "~"];
 
+// Password Character Array
+let confirmedArrayChars = [];
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function passwordOptions() {
+function passwordPrompts() {
   let passwordLength = parseInt(prompt("How many characters would you like for your password to be? [Between 8 and 128 characters]"));
 
   // Solves 8 - 128 Characters Issue - Choose Length Issue!
-  if (passwordLength >= 8 && passwordLength < 128) {
+  if (passwordLength >= 8 && passwordLength < 128 && !isNaN(passwordLength)) {
     // Ask users what prompts they'd like to include - Solves Character Types
-    let upperCase = confirm('Would you like to include Upper Case letters?');
     let lowerCase = confirm('Would you like to include Upper Case letters?');
-    let numbers = confirm('Would you like to include Upper Case letters?');
+    let upperCase = confirm('Would you like to include Upper Case letters?');
+    let number = confirm('Would you like to include Upper Case letters?');
     let specialCharacters = confirm('Would you like to include Upper Case letters?');
+  } else {
+    alert("Must choose a number between 8 and 128!")
+    return false;
   }
 
+  // Check that they have at least 1 confirmed
+  if (upperCase !== true && lowerCase !== true && numbers !== true && specialCharacters !== true) {
+    alert("Must have one prompt selected!")
+    return false;
+  }
 
+  // Adds Characters to confirmedArrayChars
+  if (lowerCase === true) {
+    confirmedArrayChars.concat(lowerletters);
+  }
+
+  if (upperCase === true) {
+    confirmedArrayChars.concat(upperletters);
+  }
+
+  if (number === true) {
+    confirmedArrayChars.concat(numbers);
+  }
+
+  if (specialCharacters === true) {
+    confirmedArrayChars.concat(specialCharss);
+  }
 }
 
 
@@ -36,9 +63,11 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+// KEEP THIS
 generateBtn.addEventListener("click", writePassword);
+
 
 /* USE CONCAT
  */
 
-/* IF thhey say all no, they have to pick */
+/* If they say all no, they have to pick */
